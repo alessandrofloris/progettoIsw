a72ba424f5e2278949e1e01acf8aa916f0cf9c37
Alessandro Floris <floris.alessandro99@gmail.com>
2023-08-13 21:24:38 +0200

    user, trip and activity models

--- travelGroup/models.py
+++ travelGroup/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.ChartField(max_length=64)
+    surname = models.ChartField(max_length=64)
+    email = models.EmailField()
+    password = models.ChartField(max_length=64) # todo da modificare(?)
+
+class Trip(models.Model):
+    name = models.ChartField(max_length=64)
+    destination = models.ChartField(max_length=64)
+    departure_date = models.DateTimeField("departure date")
+    arrival_date =  models.DateTimeField("arrival date")
+
+class Activity(models.Model):
+    description = models.ChartField(max_length=256)
+    destination = models.ChartField(max_length=64)
+    start_date = models.DateTimeField("start date")
+    end_date =  models.DateTimeField("end date")
