289a74f943551324bb3106b2179654e8c2cfe3b3
Alessandro Floris <floris.alessandro99@gmail.com>
2023-08-13 21:09:30 +0200

    new view "index" created and wired to an url

--- travelGroup/views.py
+++ travelGroup/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Questa è la pagina principale!"
+                        "Da questa pagina sarà possibile creare un nuovo gruppo di viaggio!")
