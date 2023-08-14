aa761ad57600c4608e24e2f3b2ea8e3194141b95
Alessandro Floris <floris.alessandro99@gmail.com>
2023-08-14 09:59:53 +0200

    new model migration and models bug fix

--- travelGroup/models.py
+++ travelGroup/models.py
@@ -4,19 +4,19 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.ChartField(max_length=64)
-    surname = models.ChartField(max_length=64)
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
     email = models.EmailField()
-    password = models.ChartField(max_length=64) # todo da modificare(?)
+    password = models.CharField(max_length=64) # todo da modificare(?)
 
 class Trip(models.Model):
-    name = models.ChartField(max_length=64)
-    destination = models.ChartField(max_length=64)
+    name = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
     departure_date = models.DateTimeField("departure date")
     arrival_date =  models.DateTimeField("arrival date")
 
 class Activity(models.Model):
-    description = models.ChartField(max_length=256)
-    destination = models.ChartField(max_length=64)
+    description = models.CharField(max_length=256)
+    destination = models.CharField(max_length=64)
     start_date = models.DateTimeField("start date")
     end_date =  models.DateTimeField("end date")
