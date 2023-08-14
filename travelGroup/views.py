c03cb1272209dfc6fc205a6fb64ccd7091eb9ec1
Alessandro Floris <floris.alessandro99@gmail.com>
2023-08-14 10:58:31 +0200

    add a "iMieiViaggi" view

--- travelGroup/views.py
+++ travelGroup/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 
+from .models import Trip
 # Create your views here.
 
 def index(request):
     return HttpResponse("Questa è la pagina principale!"
                         "Da questa pagina sarà possibile creare un nuovo gruppo di viaggio!")
+
+def trips(request):
+    trips = Trip.objects.all()
+    output = ", ".join([t.name for t in trips])
+    return HttpResponse(output)
