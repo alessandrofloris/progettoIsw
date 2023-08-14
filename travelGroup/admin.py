5ddfe683efe7a5948e8652fd221e16307d3d8f9b
Alessio Scanu <a.scanu35@studenti.unica.it>
2023-08-14 17:04:29 +0200

    Creating login credentials + adding comments

--- travelGroup/admin.py
+++ travelGroup/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import User, Trip, Activity
 
-
+# the following lines allow the admin users to modify 
+# the app's models from the admin site.
 admin.site.register(User)
 admin.site.register(Trip)
 admin.site.register(Activity)
