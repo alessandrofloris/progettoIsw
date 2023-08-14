40641d9d342ec24e9d5aa0df90fc97c37b83a8cf
Alessio Scanu <a.scanu35@studenti.unica.it>
2023-08-14 18:21:44 +0200

    "Invitation" and "Comment" classes creation

--- travelGroup/models.py
+++ travelGroup/models.py
@@ -30,3 +30,24 @@ class Activity(models.Model):
 
     def __str__(self):
         return self.name
+
+class Invitation(models.Model):
+    id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipient = models.EmailField(max_length=254, unique=True)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    state = models.BooleanField(default=False)
+
+    # TODO: to check
+    def __str__(self):
+        return "id: " + self.id + " state: " + self.state
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    # TODO: to check
+    def __str__(self):
+        return "id: " + self.id + " user: " + self.user + " trip: " + self.trip
