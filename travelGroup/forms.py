a8e5bdd7c7296e5fa9ba1fd12266fc9edf515357
Alessio Scanu <a.scanu35@studenti.unica.it>
2023-08-17 19:57:25 +0200

    activity formset creation

--- travelGroup/forms.py
+++ travelGroup/forms.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm
-from .models import Trip
+from django.forms import ModelForm, formset_factory
+from .models import Trip, Activity
 
 class TripForm(ModelForm):
     class Meta:
@@ -11,3 +11,16 @@ class TripForm(ModelForm):
             "departure_date" : "Departure Date",
             "arrival_date" : "Arrival Date"
         }
+
+class ActivityForm(ModelForm):
+    class Meta:
+        model = Activity
+        fields =  ["name", "description", "start_date", "end_date"]
+        labels = {
+            "name" : "Activity Name",
+            "description" : "Activity Description",
+            "start_date" : "Activity Start Date",
+            "end_date" : "Activity End Date"
+        }
+
+ActivityFormSet = formset_factory(ActivityForm, extra=1)
