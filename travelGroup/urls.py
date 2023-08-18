99d62fb4998a7fb4eb7abbf27265d06f88986d06
Alessandro Floris <floris.alessandro99@gmail.com>
2023-08-18 15:17:18 +0200

    back button behaviour and link redirect fix

--- travelGroup/urls.py
+++ travelGroup/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "travelGroup"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("mytrips", views.trips),
+    path("mytrips", views.trips, name="mytrips"),
     path("newtrip", views.newtrip),
     path("modifytrip/<int:trip_id>", views.modify_trip, name='modifytrip'),
     path("login", views.login),
