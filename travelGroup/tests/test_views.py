8ef1127a0c478ab2d38051ca8527792cd3fbf60c
Alessio Scanu <a.scanu35@studenti.unica.it>
2023-08-22 17:25:59 +0200

    Create test files

--- travelGroup/tests/test_views.py
+++ travelGroup/tests/test_views.py
@@ -19,5 +19,3 @@ from django.test import TestCase
 # test 4
 # test con sia con viaggi a cui l'utente partecipa, sia con quelli in cui non partecipa
 # vengono mostrati solamente i viaggi a cui l'utente partecipa
-
-# ++ Test dei model ++
