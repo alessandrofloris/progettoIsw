79699f5202ce73fb11e8ae810ae299a9134fef50
Alessio Scanu <a.scanu35@studenti.unica.it>
2023-08-25 19:48:07 +0200

    Update new trip acceptance test

--- travelGroup/tests/test_NewTripCreationAcceptanceTest.py
+++ travelGroup/tests/test_NewTripCreationAcceptanceTest.py
@@ -1,37 +1,25 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from django.conf import settings
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from travelGroup.models import CustomUser
 
-def test_new_trip_creation(self):
-    # user navigates to the new trip creation page
-    self.browser.get(self.live_server_url + '/newtrip')
 
-    # user fills in the form fields
-    name_input = self.browser.find_element_by_id('id_name')  # Replace with actual form field ID
-    name_input.send_keys('My Trip')
+class NewTripCreationAcceptanceTest(LiveServerTestCase):
 
-    destination_input = self.browser.find_element_by_id('id_destination')
-    destination_input.send_keys('My Destination')
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='testuser', first_name='test', last_name='user', email='testemail@email.com', password='testpassword')
+        self.driver = webdriver.Chrome()
 
-    departure_date_input = self.browser.find_element_by_id('id_departure_date')
-    departure_date_input.send_keys('2023-08-01')
+    def test_create_trip(self):
+        trip_name = self.driver.find_element(By.NAME, "name")
+        trip_name.send_keys("Test Trip")
+        trip_destination = self.driver.find_element(By.NAME, "destination")
+        trip_destination.send_keys("Test Destination")
+        trip_departure_date = self.driver.find_element(By.NAME, "departure_date")
+        trip_departure_date.send_keys("2023-07-10")
+        trip_arrival_date = self.driver.find_element(By.NAME, "arrival_date")
+        trip_arrival_date.send_keys("2023-07-15")
 
-    arrival_date_input = self.browser.find_element_by_id('id_arrival_date')
-    arrival_date_input.send_keys('2023-08-10')
 
-    # user submits the form
-    create_button = self.browser.find_element_by_name('create_button')
-    create_button.send_keys(Keys.RETURN)
-
-    # user is redirected to the mytrips page (assert the redirect)
-    self.assertIn('mytrips', self.browser.current_url)
-
-    # Checking that the trip was created and displayed on the mytrips page
-    self.assertIn('My Trip', self.browser.page_source)
-    self.assertIn('My Destination', self.browser.page_source)
-    self.assertIn('2023-08-01', self.browser.page_source)
-    self.assertIn('2023-08-10', self.browser.page_source)
 
