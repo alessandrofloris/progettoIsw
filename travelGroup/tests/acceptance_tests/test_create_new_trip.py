d422de08c19d7eb6f94faefe8290786c669ee9ce
Alessio Scanu <a.scanu35@studenti.unica.it>
2023-08-26 15:56:28 +0200

    Working acceptance test of new trip view

--- travelGroup/tests/acceptance_tests/test_create_new_trip.py
+++ travelGroup/tests/acceptance_tests/test_create_new_trip.py
@@ -1,18 +1,37 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from travelGroup.models import CustomUser
 
-class NewTripViewAcceptanceTest(LiveServerTestCase):
+class NewTripViewAcceptanceTest(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.user = CustomUser.objects.create_user(username='testuser', first_name='test', last_name='user', email='testemail@email.com', password='testpassword')
+        self.user = CustomUser.objects.create_superuser(username='testuser', first_name='test', last_name='user', email='testemail@email.com', password='testpassword')
+        self.user.save()
         self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get(self.live_server_url)
+
+        login_username = self.driver.find_element(By.NAME, "username")
+        login_username.send_keys("testuser")
+
+        login_password = self.driver.find_element(By.NAME, "password")
+        login_password.send_keys("testpassword")
+
+        login_button = self.driver.find_element(By.NAME, "login_button")
+        login_button.click()
+
+    def home(self):
+        newtrip_button = self.driver.find_element(By.NAME, "newtrip_button")
+        newtrip_button.click()
 
     # button clicked: "create_button"
     def test_create_trip(self):
-        self.driver.get("http://127.0.0.1:8000/newtrip")
+
+        self.login()
+        self.home()
 
         trip_name = self.driver.find_element(By.NAME, "name")
         trip_name.send_keys("Test Trip")
@@ -27,18 +46,15 @@ class NewTripViewAcceptanceTest(LiveServerTestCase):
         trip_arrival_date = self.driver.find_element(By.NAME, "arrival_date")
         date_value = "2023-08-16"
         self.driver.execute_script("arguments[0].value = arguments[1]", trip_arrival_date, date_value)
-
-        # TODO: add partecipant to trip
     
         trip_create_button = self.driver.find_element(By.NAME, "create_button")
         trip_create_button.click()
 
-        # cmd input
-        input("\nPress Enter to close the browser...")
-
     # button clicked: "create_add_button"
     def test_create_trip_and_add_activity(self):
-        self.driver.get("http://127.0.0.1:8000/newtrip")
+
+        self.login()
+        self.home()
 
         trip_name = self.driver.find_element(By.NAME, "name")
         trip_name.send_keys("Test Trip")
@@ -54,11 +70,6 @@ class NewTripViewAcceptanceTest(LiveServerTestCase):
         date_value = "2023-08-16"
         self.driver.execute_script("arguments[0].value = arguments[1]", trip_arrival_date, date_value)
 
-        # TODO: add partecipant to trip
-
         trip_create_button = self.driver.find_element(By.NAME, "create_add_button")
         trip_create_button.click()
-
-        # cmd input
-        input("\nPress Enter to close the browser...")
-
+    
