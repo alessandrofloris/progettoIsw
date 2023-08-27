6f8cadd125254055f70cf21cd68c85d95afaf601
Alessandro Floris <floris.alessandro99@gmail.com>
2023-08-27 16:11:56 +0200

    detail trip acceptance tests

--- travelGroup/tests/acceptance_tests/test_mytrips.py
+++ travelGroup/tests/acceptance_tests/test_mytrips.py
@@ -1,5 +1,4 @@
-import time
-
+from . import utils
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,61 +13,25 @@ class NewTripViewAcceptanceTest(StaticLiveServerTestCase):
         self.user.save()
         self.driver = webdriver.Chrome()
 
-    def login(self):
-        self.driver.get(self.live_server_url)
-
-        login_username = self.driver.find_element(By.NAME, "username")
-        login_username.send_keys("testuser")
-
-        login_password = self.driver.find_element(By.NAME, "password")
-        login_password.send_keys("testpassword")
-
-        login_button = self.driver.find_element(By.NAME, "login_button")
-        login_button.click()
-
-    def home(self):
-        newtrip_button = self.driver.find_element(By.NAME, "newtrip_button")
-        newtrip_button.click()
-
-    def create_trip(self):
-        self.home()
-
-        trip_name = self.driver.find_element(By.NAME, "name")
-        trip_name.send_keys("Test Trip")
-
-        trip_destination = self.driver.find_element(By.NAME, "destination")
-        trip_destination.send_keys("Test Destination")
-
-        trip_departure_date = self.driver.find_element(By.NAME, "departure_date")
-        date_value = "2023-08-15"
-        self.driver.execute_script("arguments[0].value = arguments[1]", trip_departure_date, date_value)
-
-        trip_arrival_date = self.driver.find_element(By.NAME, "arrival_date")
-        date_value = "2023-08-16"
-        self.driver.execute_script("arguments[0].value = arguments[1]", trip_arrival_date, date_value)
-
-        trip_create_button = self.driver.find_element(By.NAME, "create_button")
-        trip_create_button.click()
-
     def test_there_are_no_trips(self):
-        self.login()
+        utils.login(self.driver, self.live_server_url, "testuser", "testpassword")
         expected_content = "No trips to show!"
         content = self.driver.find_element(By.ID, "no-trips-title")
 
         self.assertEqual(content.text, expected_content)
 
     def test_there_are_trips(self):
-        self.login()
-        self.create_trip()
+        utils.login(self.driver, self.live_server_url, "testuser", "testpassword")
+        utils.create_trip(self.driver)
         expected_content = "Your trips"
         content = self.driver.find_element(By.ID, "trips-title")
 
         self.assertEqual(content.text, expected_content)
 
     def test_there_are_n_trips(self):
-        self.login()
-        self.create_trip()
-        self.create_trip()
+        utils.login(self.driver, self.live_server_url, "testuser", "testpassword")
+        utils.create_trip(self.driver)
+        utils.create_trip(self.driver)
 
         expected_result = 2
         content = self.driver.find_elements(By.CLASS_NAME, "single-trip-container")
